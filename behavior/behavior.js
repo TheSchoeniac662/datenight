f5d57db24f83e375f17c9b373ee581e7745f4bb9
Travis Schoen <travis.schoen@thehartford.com>
2022-10-05 10:10:56 -0400

    added intended bootstrap links and javascript widgets

--- behavior/behavior.js
+++ behavior/behavior.js
@@ -1,3 +1,14 @@
-document.ready(function(){
-	alert(`document is ready`);
+$(".carousel").carousel({
+	interval: 3000,
+	keyboard: true,
+	pause: "hover",
+	wrap: true,
+});
+
+$("#slider4").on("slide.bs.carousel", function () {
+	console.log("Slide!");
+});
+
+$("#slider4").on("slid.bs.carousel", function () {
+	console.log("Slid!");
 });
